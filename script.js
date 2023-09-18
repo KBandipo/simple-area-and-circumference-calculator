f764dfe824a7d02cc4a4abaa47d038bcfe420e3c
kennybandy <nekbandy.com>
2023-09-18 15:56:49 +0100

    Prompt the user to input the radius of a circle

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+/*
+formular for area of a circle, A = π * (r * r) or A = π * (r **2)
+Circumference of a circle, C = 2 * π * r
+Surface Area of Cube, A = 6 * (s*s) or A = 6 * (s**2)
+Volume of a cube, V = s**3 or V = s**3 
+*/
+
+//formular for area of a circle, A = π * (r * r) or A = π * (r **2)
+
+const circleRadius = Number(
+  prompt("To find the Area, A of a circle, enter the radius, r")
+);
